96bd75644b0355fd1873df53256baa00db5157c6
thetlk <jeremieboutoille@gmail.com>
2015-11-28 15:08:17 +0100

    Working multiboot \o/

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -5,19 +5,27 @@ extern kmain
 ; all information about multiboot :
 ; https://www.gnu.org/software/grub/manual/multiboot/multiboot.html
 
+%define MULTIBOOT_ALIGN 0x1
+%define MULTIBOOT_MEMINFO 0x2
+
 %define MULTIBOOT_HEADER_MAGIC 0x1BADB002
-%define MULTIBOOT_HEADER_FLAGS 0x00000003
+%define MULTIBOOT_HEADER_FLAGS (MULTIBOOT_ALIGN | MULTIBOOT_MEMINFO)
 %define MULTIBOOT_HEADER_CHECKSUM -(MULTIBOOT_HEADER_MAGIC+MULTIBOOT_HEADER_FLAGS)
 
-_start:
-    jmp start
-
-align 4
-multiboot_header:
+section .multiboot
+	align 4
+	multiboot_header:
     dd MULTIBOOT_HEADER_MAGIC
     dd MULTIBOOT_HEADER_FLAGS
     dd MULTIBOOT_HEADER_CHECKSUM
 
-start:
-    push ebx
+section .bootstrap_stack
+	stack_bottom:
+		times 16384 db 0
+	stack_top:
+
+_start:
+	cli
+	mov esp, stack_top
+	push ebx
     call kmain
--- src/kmain.c
+++ src/kmain.c
@@ -1,5 +1,14 @@
 
 void kmain()
 {
+	unsigned char *video = 0xb8000;
+	video[0] = 'Y';
+	video[1] = 0x08;
+	video[2] = 'A';
+	video[3] = 0x09;
+	video[4] = 'U';
+	video[5] = 0x0A;
+	video[6] = 'K';
+	video[7] = 0x0B;
 	while(1);
 }
