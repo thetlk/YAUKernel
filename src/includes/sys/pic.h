ad90da2b244cd4a6eb27bb6afa086c90f1179eb4
thetlk <jeremieboutoille@gmail.com>
2016-01-06 20:21:48 +0100

    clean pic code

--- src/includes/sys/pic.h
+++ src/includes/sys/pic.h
@@ -2,5 +2,15 @@
 #define PIC_H
 
 void init_pic();
+void pic_send_eoi(unsigned char irq);
+
+#define PIC_MASTER			0x20		/* IO base address for master PIC */
+#define PIC_SLAVE			0xA0		/* IO base address for slave PIC */
+#define PIC_MASTER_COMMAND	PIC_MASTER
+#define PIC_MASTER_DATA		(PIC_MASTER+1)
+#define PIC_SLAVE_COMMAND	PIC_SLAVE
+#define PIC_SLAVE_DATA		(PIC_SLAVE+1)
+
+#define PIC_EOI 0x20 /* end of interrupt */
 
 #endif
--- src/sys/pic.c
+++ src/sys/pic.c
@@ -7,24 +7,34 @@ void init_pic()
     video_print("Init PIC ... ");
 
     /* Initialisation de ICW1 */
-    outb(0x20, 0x11);
-    outb(0xA0, 0x11);
+    outb(PIC_MASTER_COMMAND, 0x11);
+    outb(PIC_SLAVE_COMMAND, 0x11);
 
     /* Initialisation de ICW2 */
-    outb(0x21, 0x20);   /* vecteur de depart = 32 */
-    outb(0xA1, 0x70);   /* vecteur de depart = 96 */
+    outb(PIC_MASTER_DATA, 0x20);   /* vecteur de depart = 32 */
+    outb(PIC_SLAVE_DATA, 0x70);   /* vecteur de depart = 96 */
 
     /* Initialisation de ICW3 */
-    outb(0x21, 0x04);
-    outb(0xA1, 0x02);
+    outb(PIC_MASTER_DATA, 0x04);
+    outb(PIC_SLAVE_DATA, 0x02);
 
     /* Initialisation de ICW4 */
-    outb(0x21, 0x01);
-    outb(0xA1, 0x01);
+    outb(PIC_MASTER_DATA, 0x01);
+    outb(PIC_SLAVE_DATA, 0x01);
 
     /* masquage des interruptions */
-    outb(0x21, 0x0);
-    outb(0xA1, 0x0);
+    outb(PIC_MASTER_DATA, 0x0);
+    outb(PIC_SLAVE_DATA, 0x0);
 
     video_print_ok();
+}
+
+void pic_send_eoi(unsigned char irq)
+{
+    if(irq >= 8)
+    {
+        outb(PIC_SLAVE_COMMAND, PIC_EOI);
+    }
+
+    outb(PIC_MASTER_COMMAND, PIC_EOI);
 }
