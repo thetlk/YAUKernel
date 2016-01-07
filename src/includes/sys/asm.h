01445e2a306e55e9965c6c2257f49bf88af012ee
thetlk <jeremieboutoille@gmail.com>
2016-01-07 15:31:24 +0100

    removes tab

--- src/includes/sys/asm.h
+++ src/includes/sys/asm.h
@@ -4,34 +4,34 @@
 #define cli() asm volatile ("cli");
 #define sti() asm volatile ("sti");
 
-#define outb(port,value) 						\
-    asm volatile ("outb %%al, %%dx"				\
-    			  :								\
-    			  : "d" (port), "a" (value)		\
-    			  :								\
-    			  );
+#define outb(port,value)              \
+    asm volatile ("outb %%al, %%dx"   \
+            :                         \
+            : "d" (port), "a" (value) \
+            :                         \
+            );
 
-#define lgdt(table)						 \
-    asm volatile ("lgdt %0"				 \
-                  :						 \
-                  : "m" (table)			 \
-                  : "memory"			 \
-                 );						 \
+#define lgdt(table)                      \
+    asm volatile ("lgdt %0"              \
+                  :                      \
+                  : "m" (table)          \
+                  : "memory"             \
+                 );                      \
     asm volatile ("movw $0x10, %%ax   ;" \
               "movw %%ax, %%ds    ;"     \
-              "movw %%ax, %%es    ;"	 \
-              "movw %%ax, %%fs    ;"	 \
-              "movw %%ax, %%gs    ;"	 \
-              "movw %%ax, %%ss    ;"	 \
-              "ljmp $0x08, $next  ;"	 \
-              "next:              ;"	 \
+              "movw %%ax, %%es    ;"     \
+              "movw %%ax, %%fs    ;"     \
+              "movw %%ax, %%gs    ;"     \
+              "movw %%ax, %%ss    ;"     \
+              "ljmp $0x08, $next  ;"     \
+              "next:              ;"     \
              :::);
 
-#define lidt(table) 				\
-    asm volatile ("lidtl %0"		\
-                  :					\
-                  : "m" (table)		\
-                  : "memory"		\
+#define lidt(table)             \
+    asm volatile ("lidtl %0"    \
+                  :             \
+                  : "m" (table) \
+                  : "memory"    \
                  );
 
 #endif
