766851c931b1432fdcb399b9099c269403aa68d5
thetlk <jeremieboutoille@gmail.com>
2016-01-10 22:17:34 +0100

    add memset

--- src/libc/mem.c
+++ src/libc/mem.c
@@ -11,3 +11,15 @@ void *memcpy(unsigned char *dst, unsigned char *src, unsigned int l)
 	
 	return ret;
 }
+
+void *memset(unsigned char *dst, int c, unsigned int l)
+{
+	unsigned char *ret = dst;
+
+	while(l--)
+	{
+		*dst++ = c;
+	}
+
+	return ret;
+}
