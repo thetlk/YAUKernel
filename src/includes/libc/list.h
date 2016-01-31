e4ad855ad5f5275b190639c22caf8d6b0076ccef
thetlk <jeremieboutoille@gmail.com>
2016-01-31 18:02:25 +0100

    add foreach and remove on generic list

--- src/includes/libc/list.h
+++ src/includes/libc/list.h
@@ -2,7 +2,10 @@
 #define LIST_H
 
 /*
-    struct page *p1, *p2;
+    struct page *p1 = NULL;
+    struct page *p2 = NULL;
+    struct page *p3 = NULL;
+    struct page *current = NULL;
     LIST_HEAD(page_list_type, page) page_list = LIST_HEAD_INITIALIZER(page_list);
 
     LIST_INIT(&page_list);
@@ -13,17 +16,28 @@
     LIST_INSERT_HEAD(&page_list, p1, list);
 
     p2 = malloc(sizeof(struct page));
-    p2->physaddr = (void*) 0x0;
+    p2->physaddr = (void*) 0x1000;
     p2->virtaddr = (void*) 0x1337babe;
     LIST_INSERT_AFTER(p1, p2, list);
 
-    LIST_REMOVE_HEAD(&page_list, list);
+    p3 = malloc(sizeof(struct page));
+    p3->physaddr = (void*) 0x1;
+    p3->virtaddr = (void*) 0x2;
+    LIST_INSERT_AFTER(p1, p3, list);
+
+    LIST_REMOVE(&page_list, p3, page, list);
+
+    LIST_FOREACH(current, &page_list, list)
+    {
+        printf("%p -> %p\n", current->virtaddr, current->physaddr);
+    }
+
     free(p1);
+    free(p2);
+    free(p3);
 */
 
-#define LIST_FIRST(head) ((head)->first)
-#define LIST_EMPTY(head) ((head)->first == 0)
-
+// Declarations
 #define LIST_HEAD(headname, type)       \
     struct headname                     \
     {                                   \
@@ -38,10 +52,23 @@
         struct type *next;  \
     }
 
-#define LIST_INIT(head)     \
-    LIST_FIRST(head) = 0
+// Functions
+#define LIST_EMPTY(head) ((head)->first == 0)
+#define LIST_FIRST(head) ((head)->first)
+
+#define LIST_INIT(head) LIST_FIRST(head) = 0
 
 #define LIST_NEXT(element, field) (element->field.next)
+
+#define LIST_FOREACH(var, head, field)      \
+    for((var) = LIST_FIRST((head));         \
+        (var);                              \
+        (var) = LIST_NEXT((var), field))
+
+#define LIST_FOREACH_FROM(var, head, field)             \
+    for((var) = ((var) ? (var) : LIST_FIRST((head)));   \
+        (var);                                          \
+        (var) = LIST_NEXT((var), field))
 
 #define LIST_INSERT_HEAD(head, element, field)          \
     LIST_NEXT((element), field) = LIST_FIRST((head));   \
@@ -53,5 +80,21 @@
 
 #define LIST_REMOVE_HEAD(head, field)                           \
     LIST_FIRST((head)) = LIST_NEXT(LIST_FIRST((head)), field)
+
+#define LIST_REMOVE_AFTER(element, field)                                       \
+    LIST_NEXT((element), field) = LIST_NEXT(LIST_NEXT((element), field), field)
+
+#define LIST_REMOVE(head, element, type, field)                     \
+    if(LIST_FIRST((head)) == ((element)))                           \
+    {                                                               \
+        LIST_REMOVE_HEAD((head), field);                            \
+    } else {                                                        \
+        struct type *current_element = LIST_FIRST((head));          \
+        while(LIST_NEXT(current_element, field) != (element))       \
+        {                                                           \
+            current_element = LIST_NEXT(current_element, field);    \
+        }                                                           \
+        LIST_REMOVE_AFTER(current_element, field);                  \
+    }
 
 #endif
