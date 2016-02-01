3e0cf10777386d926ff732bb8a57ba2828dc689a
thetlk <jeremieboutoille@gmail.com>
2016-02-01 22:46:30 +0100

    list: add 'last' element inside head list

--- src/includes/libc/list.h
+++ src/includes/libc/list.h
@@ -42,9 +42,10 @@
     struct headname                     \
     {                                   \
         struct type *first;             \
+        struct type *last;              \
     }
 
-#define LIST_HEAD_INITIALIZER(head) { 0 }
+#define LIST_HEAD_INITIALIZER(head) { 0, 0 }
 
 #define LIST_ENTRY(type)    \
     struct                  \
@@ -55,10 +56,21 @@
 // Functions
 #define LIST_EMPTY(head) ((head)->first == 0)
 #define LIST_FIRST(head) ((head)->first)
+#define LIST_LAST(head) ((head)->last)
 
-#define LIST_INIT(head) LIST_FIRST(head) = 0
+#define LIST_INIT(head)     \
+    LIST_FIRST((head)) = 0; \
+    LIST_LAST((head)) = 0
 
 #define LIST_NEXT(element, field) (element->field.next)
+
+#define LIST_CONCAT(head1, head2, field)                            \
+    if(!LIST_EMPTY(head2))                                          \
+    {                                                               \
+        LIST_NEXT(LIST_LAST((head1)), field) = LIST_FIRST((head2)); \
+        LIST_LAST((head1)) = LIST_LAST((head2));                    \
+    }                                                               \
+    LIST_INIT((head2))
 
 #define LIST_FOREACH(var, head, field)      \
     for((var) = LIST_FIRST((head));         \
@@ -70,19 +82,36 @@
         (var);                                          \
         (var) = LIST_NEXT((var), field))
 
-#define LIST_INSERT_HEAD(head, element, field)          \
-    LIST_NEXT((element), field) = LIST_FIRST((head));   \
-    LIST_FIRST((head)) = element
+#define LIST_INSERT_HEAD(head, element, field)                  \
+    if((LIST_NEXT((element), field) = LIST_FIRST((head))) == 0) \
+    {                                                           \
+        LIST_LAST((head)) = (element);                          \
+    }                                                           \
+    LIST_FIRST((head)) = (element)
 
-#define LIST_INSERT_AFTER(helement, element, field)             \
-    LIST_NEXT((element), field) = LIST_NEXT((helement), field); \
+#define LIST_INSERT_AFTER(head, helement, element, field)                   \
+    if((LIST_NEXT((element), field) = LIST_NEXT((helement), field)) == 0)   \
+    {                                                                       \
+        LIST_LAST((head)) = (element);                                      \
+    }                                                                       \
     LIST_NEXT((helement), field) = (element)
 
-#define LIST_REMOVE_HEAD(head, field)                           \
-    LIST_FIRST((head)) = LIST_NEXT(LIST_FIRST((head)), field)
+#define LIST_INSERT_TAIL(head, element, field)          \
+    LIST_NEXT((element), field) = 0;                    \
+    LIST_NEXT((LIST_LAST((head))), field) = element;    \
+    LIST_LAST((head)) = element
 
-#define LIST_REMOVE_AFTER(element, field)                                       \
-    LIST_NEXT((element), field) = LIST_NEXT(LIST_NEXT((element), field), field)
+#define LIST_REMOVE_HEAD(head, field)                                                           \
+    if(!LIST_EMPTY((head)) && (LIST_FIRST((head)) = LIST_NEXT(LIST_FIRST((head)), field)) == 0) \
+    {                                                                                           \
+        LIST_LAST((head)) = 0;                                                                  \
+    }
+
+#define LIST_REMOVE_AFTER(head, element, field)                                       \
+    if((LIST_NEXT((element), field) = LIST_NEXT(LIST_NEXT((element), field), field)) == 0) \
+    {\
+        LIST_LAST((head)) = element; \
+    }
 
 #define LIST_REMOVE(head, element, type, field)                     \
     if(LIST_FIRST((head)) == ((element)))                           \
@@ -94,7 +123,7 @@
         {                                                           \
             current_element = LIST_NEXT(current_element, field);    \
         }                                                           \
-        LIST_REMOVE_AFTER(current_element, field);                  \
+        LIST_REMOVE_AFTER(head, current_element, field);            \
     }
 
 #endif
